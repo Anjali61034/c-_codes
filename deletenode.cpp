55a0da6df94630aabb7fea0618805d89f70547c5
Anjali61034 <153043480+Anjali61034@users.noreply.github.com>
2024-05-10 15:16:30 +0530

    Update deletenode.cpp

--- deletenode.cpp
+++ deletenode.cpp
@@ -1,129 +1,143 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node
-{
+// Node class
+class Node {
 public:
-  int data;
-  Node *next;
+    int data;
+    Node* next;
+
+    // Constructor
+    Node(int newData) {
+        data = newData;
+        next = nullptr;
+    }
 };
 
-class LinkedList
-{
+// Linked List class
+class LinkedList {
 private:
-  Node * head;
+    Node* head;
+
 public:
-  LinkedList ()
-  {				// constructor
-    head = NULL;
-  }
-  int calcSize ();
-  void deleteStart ();
-  void deleteEnd ();
-  void deleteNthNode (int n);
-  void display ();
-  void insert (int data);
+    // Constructor
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Function to insert a node at the beginning
+    void insertAtHead(int newData) {
+        Node* newNode = new Node(newData);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Function to delete a node from the head
+    void deleteFromHead() {
+        if (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        } else {
+            cout << "List is empty. Cannot delete from head." << endl;
+        }
+    }
+
+    // Function to delete a node from the tail
+    void deleteFromTail() {
+        if (!head) {
+            cout << "List is empty. Cannot delete from tail." << endl;
+            return;
+        }
+
+        if (!head->next) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+
+        Node* prev = nullptr;
+        Node* current = head;
+        while (current->next) {
+            prev = current;
+            current = current->next;
+        }
+        delete current;
+        prev->next = nullptr;
+    }
+
+    // Function to delete a node from a specific position
+    void deleteFromPosition(int pos) {
+        if (!head) {
+            cout << "List is empty. Cannot delete from position." << endl;
+            return;
+        }
+
+        if (pos < 0) {
+            cout << "Invalid position. Cannot delete from position." << endl;
+            return;
+        }
+
+        if (pos == 0) {
+            deleteFromHead();
+            return;
+        }
+
+        Node* current = head;
+        Node* prev = nullptr;
+        int count = 0;
+        while (current && count < pos) {
+            prev = current;
+            current = current->next;
+            count++;
+        }
+
+        if (!current) {
+            cout << "Position out of range. Cannot delete from position." << endl;
+            return;
+        }
+
+        prev->next = current->next;
+        delete current;
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 
-void LinkedList::deleteNthNode (int n)
-{
-  Node *temp = head;
-  Node *previous;
+int main() {
+    LinkedList list;
 
+    // Insert some elements
+    list.insertAtHead(3);
+    list.insertAtHead(2);
+    list.insertAtHead(1);
 
-  int size = calcSize ();
+    // Display the list
+    cout << "Original List: ";
+    list.display();
 
-  if (n < 1 || n > size)
-    {
-      cout << "\nEnter valid position\n";
-      return;
-    }
+    // Delete from head
+    list.deleteFromHead();
+    cout << "After deleting from head: ";
+    list.display();
 
-  // if first node has to be deleted 
-  if (n == 1)
-    {
-      head = head->next;
-      cout << "\nValue deleted: " << temp->data << endl;
-      delete (temp);
-      return;
-    }
+    // Delete from tail
+    list.deleteFromTail();
+    cout << "After deleting from tail: ";
+    list.display();
 
-  //traverse till the nth node 
-  while (--n)
-    {
-      // store previous link as we need to change its next val 
-      previous = temp;
-      temp = temp->next;
-    }
+    // Delete from specific position
+    list.deleteFromPosition(1); // Deleting the node at position 1
+    cout << "After deleting from position 1: ";
+    list.display();
 
-  // previous node's next changed to nth node's next
-  previous->next = temp->next;
-
-  cout << "Value deleted: " << temp->data << endl;
-  delete (temp);
-}
-
-int LinkedList::calcSize ()
-{
-  Node *node = new Node ();
-  node = head;
-
-  int size = 0;
-
-  while (node != NULL)
-    {
-      node = node->next;
-      size++;
-    }
-  return size;
-}
-
-void LinkedList::insert (int data)
-{
-
-  Node *newNode = new Node ();
-
-  newNode->data = data;
-  newNode->next = head;
-
-  // assigned head to newNode
-  head = newNode;
-}
-
-void LinkedList::display ()
-{
-
-  Node *temp = new Node ();
-  temp = head;
-
-  cout << "Linked List : ";
-
-  //as linked list will end when Node is Null
-  while (temp != NULL)
-    {
-      cout << temp->data << " ";
-      temp = temp->next;
-    }
-  cout << endl;
-}
-
-int main ()
-{
-  LinkedList *list = new LinkedList ();
-  list->insert (25);
-  list->insert (24);
-  list->insert (24);
-  list->insert (22);
-  list->insert (21);
-  list->insert (20);
-
-  list->display ();
-
-  list->deleteNthNode (2);
-
-  list->deleteNthNode (4);
-
-  list->display ();
-
-  return 0;
+    return 0;
 }
